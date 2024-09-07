5470471614d29ebf9fdf41543eeb2ff14847bae6
Ademola Ademeso <desmondademeso@gmail.com>
2024-09-07 21:58:07 +0100

    Updated NavBar responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,32 +4,32 @@ export const NavBar = () => {
       <p className="text-white text-4xl md:text-5xl font-bold">MusicAI</p>
       <a
         href="#about"
-        className="text-white text-2xl md:text-4xl hover:text-red-400 transition-colors"
+        className="text-white text-sm sm:text-lg md:text-4xl hover:text-red-400 transition-colors"
       >
         About
       </a>
       <a
         href="#features"
-        className="text-white text-2xl md:text-4xl hover:text-red-400 transition-colors"
+        className="text-white text-sm sm:text-lg md:text-4xl hover:text-red-400 transition-colors"
       >
         Features
       </a>
       <a
         href="#contact"
-        className="text-white text-2xl md:text-4xl hover:text-red-400 transition-colors"
+        className="text-white text-sm sm:text-lg md:text-4xl hover:text-red-400 transition-colors"
       >
         Contact
       </a>
       <a
         href="#video"
-        className="text-white text-2xl md:text-4xl hover:text-red-400 transition-colors"
+        className="text-white text-sm sm:text-lg md:text-4xl hover:text-red-400 transition-colors"
       >
         Video
       </a>
       <a
         href="https://musicai-client.vercel.app/"
         target="_blank"
-        className="text-red-800 bg-white rounded-full px-3 pb-1 text-2xl md:text-4xl hover:bg-red-800 hover:text-white transition-colors"
+        className="text-red-800 bg-white rounded-full px-3 pb-1 text-sm sm:text-lg md:text-4xl hover:bg-red-800 hover:text-white transition-colors"
       >
         Discover
       </a>
